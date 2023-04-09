f832f32e7cbee0b9a742d3f5789e8c8207afeb55
saurabh kumar <saurabhkumar9493@gmail.com>
2023-04-09 23:17:52 +0530

    array

---
+++ src/Array_problem1.java
@@ -0,0 +1,13 @@
+public class Array_problem1 {
+    public static void main(String[] args) {
+        int arr [] = {1,5,3,5,6};
+        // System.out.println(arr[0]+arr[1]+arr[2]);
+
+          int sum = 0;
+        for (int i =0; i<arr.length; i++){
+             sum = sum+arr[i];
+
+        }
+        System.out.println(sum);
+    }
+}
